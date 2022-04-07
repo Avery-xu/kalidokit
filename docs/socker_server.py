b937d1038f95c6d84a5603543d08e6076faaf3b0
xylem <xujunzz@sjtu.edu.cn>
2022-04-07 09:25:59 +0800

    Add a socket server to replace the weak httpserver.

---
+++ docs/socker_server.py
@@ -0,0 +1,28 @@
+import socketio
+import eventlet
+from multiprocessing import Queue
+
+q = Queue()
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+@sio.event
+def connect(sid):
+    print('connect ', sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+@sio.event
+def post(sid, data):
+    q.put(data)
+    # print("post")
+
+@sio.event
+def get(sid):
+    if (q.qsize() > 0):
+        sio.emit("get", q.get())
+        # print("get")
+
+eventlet.wsgi.server(eventlet.listen(('', 80)), app)
